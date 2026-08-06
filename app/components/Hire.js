e35b3d79c7cf591a75f494cd3cfc8e3ab13acc69
Hamza-code88 <harper969798@gmail.com>
2026-08-06 20:25:25 +0500

    link added

--- app/components/Hire.js
+++ app/components/Hire.js
@@ -20,14 +20,14 @@ export const Hire = () => {
       <div className='animate-marquee cursor-pointer'>
         {/* First Set */}
         <div className='flex justify-around w-1/2'>
-            <a href="https://www.upwork.com/" target="_blank" rel="noreferrer" className='text-white text-2xl font-bold mx-8 hover:text-gray-200 transition'>Upwork</a>
+            <a href="https://www.upwork.com/freelancers/~01df46cd7c20bc1397?mp_source=share" target="_blank" rel="noreferrer" className='text-white text-2xl font-bold mx-8 hover:text-gray-200 transition'>Upwork</a>
             <a href="https://www.linkedin.com/in/hamza-khan-0a5419336" target="_blank" rel="noreferrer" className='text-white text-2xl font-bold mx-8 hover:text-gray-200 transition'>LinkedIn</a>
             <a href="https://wa.me/923494427822" target="_blank" rel="noreferrer" className='text-white text-2xl font-bold mx-8 hover:text-gray-200 transition'>WhatsApp</a>
             <a href="mailto:hamza@example.com" className='text-white text-2xl font-bold mx-8 hover:text-gray-200 transition'>Email</a>
         </div>
         {/* Second Set (Duplicate for seamless loop) */}
         <div className='flex justify-around w-1/2'>
-            <a href="https://www.upwork.com/" target="_blank" rel="noreferrer" className='text-white text-2xl font-bold mx-8 hover:text-gray-200 transition'>Upwork</a>
+            <a href="https://www.upwork.com/freelancers/~01df46cd7c20bc1397?mp_source=share" target="_blank" rel="noreferrer" className='text-white text-2xl font-bold mx-8 hover:text-gray-200 transition'>Upwork</a>
             <a href="https://www.linkedin.com/in/hamza-khan-0a5419336" target="_blank" rel="noreferrer" className='text-white text-2xl font-bold mx-8 hover:text-gray-200 transition'>LinkedIn</a>
             <a href="https://wa.me/923494427822" target="_blank" rel="noreferrer" className='text-white text-2xl font-bold mx-8 hover:text-gray-200 transition'>WhatsApp</a>
             <a href="mailto:hamza@example.com" className='text-white text-2xl font-bold mx-8 hover:text-gray-200 transition'>Email</a>
